84d925fd465a5b1aa2d144d3f8bcc707ec73f8b4
Seiya Nuta <nuta@seiya.me>
2018-05-01 17:13:13 +0900

    wip

--- kernel/elf.c
+++ kernel/elf.c
@@ -2,5 +2,69 @@
 
 
 void elf_create_process(const void *image, size_t length, pager_t *pager, void *pager_arg) {
+    Elf64_Ehdr *ehdr;
+
+    ehdr = (Elf64_Ehdr *) data;
+
+    /* check out the magic number */
+    if (strcmp(ehdr->e_ident[0] != ELF_MAGIC) != 0) {
+        INFO("%s: invalid elf magic, skipping...", image);
+        return;
+    }
+
+    if (ehdr->e_ident[4] != ELFCLASS64 || ehdr->e_machine != EM_X86_64) {
+        INFO("%s: unsupported type, skipping...", image);
+        return;
+    }
+
+    Elf64_Ehdr *ehdr;
+    Elf64_Phdr *phdr;
+    int i;
+
+    /* load and validate the header */
+    resea::interfaces::fs::call_read(fs,
+         file, 0, sizeof(Elf64_Ehdr) + (sizeof(Elf64_Phdr) * PHDR_MAX),
+         &r, &data, &size);
+
+    if (r != OK)
+        return r;
+
+    INFO("verifying the ELF header");
+    if (elf_validate(data, ELF_ARCH_X86_64 /* FIXME */) != OK) {
+        INFO("invalid ELF header, aborting");
+        return E_INVALID;
+    }
+
+    ehdr = (Elf64_Ehdr *) data;
+    if (ehdr->e_phoff == 0) {
+        WARN("tried to load a invalid file as an executable");
+        return E_INVALID;
+    }
+
+    /* load program headers */
+    INFO("reading program headers");
+    for (i=0; i < ehdr->e_phnum && i < PHDR_MAX; i++) {
+        phdr = (Elf64_Phdr *) ((uintptr_t)  data + ehdr->e_phoff + (ehdr->e_phentsize * i));
+
+        if (phdr->p_type == PT_LOAD) {
+            uint8_t flags = 0;
+            flags |= (phdr->p_flags & PF_R)? resea::interfaces::memory::MAP_READ  : 0;
+            flags |= (phdr->p_flags & PF_W)? resea::interfaces::memory::MAP_WRITE : 0;
+            flags |= (phdr->p_flags & PF_X)? resea::interfaces::memory::MAP_EXEC  : 0;
+
+            /* map the area to the virtual memory space */
+            INFO("mapping a pager: vaddr=%p, size=%d type=%c%c%c",
+                 phdr->p_vaddr, phdr->p_filesz,
+                 (phdr->p_flags & PF_R)? 'R' : '-',
+                 (phdr->p_flags & PF_W)? 'W' : '-',
+                 (phdr->p_flags & PF_X)? 'X' : '-');
+
+            resea::interfaces::memory::call_map(memory_ch,
+                 group, phdr->p_vaddr, phdr->p_filesz << 8 | flags,
+                 fs, file, phdr->p_offset,
+                 &r);
+        }
+    }
+
 
 }
--- kernel/elf.h
+++ kernel/elf.h
@@ -4,6 +4,63 @@
 #include "types.h"
 #include "memory.h"
 
+enum elf_archtype{
+  ELF_ARCH_X86_64
+};
+
+#define EI_NIDENT   16
+#define EI_MAG0     0x7f
+#define EI_MAG1     'E'
+#define EI_MAG2     'L'
+#define EI_MAG3     'F'
+#define ELFCLASS64  2
+#define EM_X86_64   62
+#define PT_LOAD     1
+#define PF_R        4
+#define PF_W        2
+#define PF_X        1
+
+struct elf64_ehdr{
+  uint8_t  e_ident[EI_NIDENT];
+  uint16_t e_type;
+  uint16_t e_machine;
+  uint32_t e_version;
+  uint64_t e_entry;
+  uint64_t e_phoff;
+  uint64_t e_shoff;
+  uint32_t e_flags;
+  uint16_t e_ehsize;
+  uint16_t e_phentsize;
+  uint16_t e_phnum;
+  uint16_t e_shentsize;
+  uint16_t e_shnum;
+  uint16_t e_shstrndx;
+};
+
+struct elf64_phdr {
+  uint32_t p_type;
+  uint32_t p_flags;
+  uint64_t p_offset;
+  uint64_t p_vaddr;
+  uint64_t p_paddr;
+  uint64_t p_filesz;
+  uint64_t p_memsz;
+  uint64_t p_align;
+};
+
+struct elf64_shdr {
+   uint32_t sh_name;
+   uint32_t sh_type;
+   uint64_t sh_flags;
+   uint64_t sh_addr;
+   uint64_t sh_offset;
+   uint64_t sh_size;
+   uint32_t sh_link;
+   uint32_t sh_info;
+   uint64_t sh_addralign;
+   uint64_t sh_entsize;
+};
+
 void elf_create_process(const void *image, size_t length, pager_t *pager, void *pager_arg);
 
 #endif
