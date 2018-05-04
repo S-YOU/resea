c22bef25db856e0b78c0f30e53d4f63dccae0230
Seiya Nuta <nuta@seiya.me>
2018-05-04 15:55:47 +0900

    wip

--- servers/fatfs/target/x64/debug/deps/fatfs-7bc36e899b260e61.d
+++
@@ -1,7 +0,0 @@
-../../servers/fatfs/fatfs.o: src/lib.rs
-
-/Users/seiya/dev/resea/servers/fatfs/target/x64/debug/deps/libfatfs-7bc36e899b260e61.rlib: src/lib.rs
-
-/Users/seiya/dev/resea/servers/fatfs/target/x64/debug/deps/fatfs-7bc36e899b260e61.d: src/lib.rs
-
-src/lib.rs:
--- servers/fatfs/target/x64/debug/libfatfs.d
+++
@@ -1,1 +0,0 @@
-/Users/seiya/dev/resea/servers/fatfs/target/x64/debug/libfatfs.rlib: /Users/seiya/dev/resea/servers/fatfs/src/lib.rs
